9c3556e9f2f38f74df79220d5fa48cd3b9781354
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-05-17 13:02:42 +0530

    Added Agent

---
+++ AIAgentPOC/AIAgentPOC/AIAgentPluginDemo/PizzaAIAgent/PizzaAIAgent.cs
@@ -0,0 +1,111 @@
+﻿using AIAgentPOC.Model;
+using AIAgentPOC.SemanticKernal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIAgentPOC.AIAgentPluginDemo.PizzaAIAgent
+{
+    public class PizzaAIAgent
+    {
+        IConfiguration _configuration;
+        IAIConnectorService _aIConnectorService;
+        Kernel _Kernel;
+
+        public PizzaAIAgent (IConfiguration configuration, IAIConnectorService aIConnectorService, Kernel kernel)
+        {
+            _configuration = configuration;
+            _aIConnectorService = aIConnectorService;
+            _Kernel = CreateKernel();
+        }
+
+        private Kernel CreateKernel()
+        {
+            return _aIConnectorService.BuildChatCompletionKernel(_configuration);
+        }
+
+        private AIAgentInput GetAgentProfile()
+        {
+            AIAgentInput aIAgentInput = new AIAgentInput();
+            aIAgentInput.Instructions = """
+                You are a friendly assistant who will take pizza order from user. 
+                Ask user to select pizza, toppings and size beforing adding to cart.
+                User can order mulitple pizza.
+                Display selected cart before proceding order.
+                Ask user for adding/removing pizza from cart before ordering.
+                Require user approval for completing order
+                If the user doesn't provide enough information for you to complete a order, you will keep asking questions until you have
+                enough information to complete the order.
+                Display sentence on new line.
+                """;
+
+            aIAgentInput.Name = "Pizza Order AI Agent ";
+            aIAgentInput.Kernel = _Kernel;
+
+            return aIAgentInput;
+        }
+
+        private ChatCompletionAgent CreateAgent()
+        {
+            AIAgentInput agentInput = GetAgentProfile();
+            ChatCompletionAgent agent = new()
+            {
+                Instructions = agentInput.Instructions,
+                Name = agentInput.Name,
+                Kernel = agentInput.Kernel,
+            };
+
+            return agent;
+        }
+
+        public async Task StartPizzaOrder()
+        {
+            ChatCompletionAgent agent = CreateAgent();
+
+            ChatHistoryAgentThread chatHistoryAgentThread = new ChatHistoryAgentThread();
+
+            bool isComplete = false;
+
+            do
+            {
+                Console.WriteLine();
+                Console.Write("User> ");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    continue;
+                }
+
+                if (input.Trim().Equals("EXIT", StringComparison.OrdinalIgnoreCase))
+                {
+                    isComplete = true;
+                    break;
+                }
+
+                var message = new ChatMessageContent(AuthorRole.User, input);
+
+                Console.WriteLine();
+                Console.WriteLine("Assistant> Thinking.......");
+                Console.WriteLine();
+
+                await foreach (StreamingChatMessageContent response in agent.InvokeStreamingAsync(message, chatHistoryAgentThread))
+                {
+                    Console.Write(response.Content);
+                }
+
+                Console.WriteLine();
+
+            } while (!isComplete);
+
+            await chatHistoryAgentThread.DeleteAsync();
+        }
+       
+    }
+}
