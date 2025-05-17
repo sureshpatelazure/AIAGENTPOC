b4ea650bf6158dc1ffa3693f41f0d766d35db016
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-05-17 12:35:12 +0530

    Added Pizza Cart

--- AIAgentPOC/AIAgentPOC/AIAgentPluginDemo/Core/PizzaModel.cs
+++ AIAgentPOC/AIAgentPOC/AIAgentPluginDemo/Core/PizzaModel.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AIAgentPOC.AIAgentPluginDemo.Core
+namespace AIAgentPOC.AIAgentPluginDemo.Core.Model
 {
-    internal class PizzaModel
+    public class Pizza
     {
+        public long Id { get; set; }
+        public string? Name { get; set;}
+        public string? Description { get; set;}
+        public decimal Price { get; set;}
     }
+
+    public class PizzaTopping
+    {
+        public long Id { get; set; }
+        public string? Name { get; set;}
+    }
+
+    public class PizzaSize
+    {
+        public long Id { get; set; }
+        public string? Size { get; set; }
+    }
+
+    public class PizzaCart
+    {
+        public long Id { get; set;}
+        public Pizza? Pizza { get; set;}
+        public PizzaSize? Size { get; set; }
+        public List<PizzaTopping>? Toppings { get; set; }
+        public int Quantity { get; set; }
+
+        public string? SpecialInstruction { get; set; }
+       
+    } 
 }
---
+++ AIAgentPOC/AIAgentPOC/AIAgentPluginDemo/Plugin/PizzaPlugin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AIAgentPOC.AIAgentPluginDemo.Core.Service;
+using AIAgentPOC.AIAgentPluginDemo.Core.Model;
+using Microsoft.SemanticKernel;
+using System.ComponentModel;
+
+namespace AIAgentPOC.AIAgentPluginDemo.Plugin
+{
+    public class PizzaPlugin
+    {
+        PizzaService _pizzaService = new PizzaService();
+
+        [KernelFunction("get_pizza_menu")]
+        [Description("This Function will return List of Pizza Menus to users.")]
+        public List<Pizza> GetPizzMenu()
+        {
+            return _pizzaService.GetPizzaMenu();
+        }
+
+        [KernelFunction("add_pizza_to_cart")]
+        [Description("Add a pizza to the user's cart; returns updated the cart")]
+        public List<PizzaCart> AddPizzaToCart(
+            Pizza pizza,
+            PizzaSize pizzasize,
+            List<PizzaTopping> pizzatoppings,
+            int pizzaquantity = 1,
+            string specialInstruction = ""
+            ) {
+
+            return _pizzaService.AddPizzaToCart(pizza, pizzasize, pizzatoppings, pizzaquantity, specialInstruction);
+        }
+
+        [KernelFunction("remove_pizza_from_cart")]
+        [Description("Remove a pizza from the user's cart; returns updated the cart")]
+        public string RemovePizzaFromCart(int cartId)
+        {
+            _pizzaService.RemovePizzaFromCart(cartId);
+            return "Pizza removed with cartid:" + cartId;
+        }
+
+        [KernelFunction("get_cart")]
+        [Description("Returns the user's current cart")]
+        public List<PizzaCart> GetCart()
+        {
+            return _pizzaService.GetCart(); 
+        }
+
+        [KernelFunction("checkout")]
+        [Description("Checkouts the user's cart; this function will retrieve the payment from the user and complete the order.")]
+        public string Checkout(int cartId)
+        {
+            _pizzaService.Checkout(cartId);
+
+            return "Pizza Order Completed with CartId " + cartId + ". Thank you for ordering pizza";
+        }
+    }
+}
