1a71137bb33ad79bf79c795a071cf80a89401b7b
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-05-24 15:54:16 +0530

    Cleanup code

--- AIAgentPOC/AIAgentLib/SemanticKernalService/OllamaKernelChatCompletionService.cs
+++ AIAgentPOC/AIAgentLib/SemanticKernalService/OllamaKernelChatCompletionService.cs
@@ -7,31 +7,29 @@ namespace AIAgentLib.SemanticKernalService
     {
         public Kernel BuildChatCompletion<T>(T connectorConfiguration) where T : AIConnectorServiceConfiguration
         {
-            if (connectorConfiguration is OllamaConnectorServiceConfiguration ollama)
-            {
-                var builder = Kernel.CreateBuilder();
-                builder.AddOllamaChatCompletion(ollama.ModelId, new Uri(ollama.Uri));
-                return builder.Build();
-            }
-           else
-            {
-                throw new ArgumentException($"Unsupported model type: {typeof(T).Name}");
-            }
+            var builder = CreateOllamaKernelBuilder(connectorConfiguration);
+            return builder.Build();
         }
 
         public Kernel BuildChatCompletion<T>(T connectorConfiguration, List<object> Plugins) where T : AIConnectorServiceConfiguration
+        {
+            var builder = CreateOllamaKernelBuilder(connectorConfiguration);
+
+            foreach (var plugin in Plugins)
+            {
+                builder.Plugins.AddFromObject(plugin);
+            }
+
+            return builder.Build();
+        }
+
+        private IKernelBuilder CreateOllamaKernelBuilder<T>(T connectorConfiguration) where T : AIConnectorServiceConfiguration
         {
             if (connectorConfiguration is OllamaConnectorServiceConfiguration ollama)
             {
                 var builder = Kernel.CreateBuilder();
                 builder.AddOllamaChatCompletion(ollama.ModelId, new Uri(ollama.Uri));
-
-                foreach (var plugin in Plugins)
-                {
-                    builder.Plugins.AddFromObject(plugin);
-                }
-
-                return builder.Build();
+                return builder;
             }
             else
             {
