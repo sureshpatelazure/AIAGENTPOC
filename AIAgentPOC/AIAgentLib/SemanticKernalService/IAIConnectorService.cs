1f6c6d239f4ba398d2c8eb216cbcb2a4e5bc8d60
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-05-24 11:31:01 +0530

    Added IAIConnectorService Interface

---
+++ AIAgentPOC/AIAgentLib/SemanticKernalService/IAIConnectorService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+namespace AIAgentLib.SemanticKernalService
+{
+    public interface IAIConnectorService
+    {
+        public Kernel BuildChatCompletion(IConfiguration configuration);
+        public Kernel BuildChatCompletion(IConfiguration configuration, List<Object> Plugins);
+
+    }
+}
---
+++ AIAgentPOC/AIAgentLib/SemanticKernalService/OllamaKernelService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+namespace AIAgentLib.SemanticKernalService
+{
+    public class OllamaKernelService : IAIConnectorService
+    {
+        public Kernel BuildChatCompletion(IConfiguration configuration)
+        {
+            var builder = Kernel.CreateBuilder();
+            return builder.Build();
+        }
+
+        public Kernel BuildChatCompletion(IConfiguration configuration, List<object> Plugins)
+        {
+            var builder = Kernel.CreateBuilder();
+            return builder.Build();
+        }
+    }
+}
