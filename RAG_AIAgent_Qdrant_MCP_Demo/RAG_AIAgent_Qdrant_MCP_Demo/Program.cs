bb254c261bb7c6920cbd9f0ff79ffd18a05efcfa
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-07-05 18:52:32 +0530

    RAG is working

--- RAG_AIAgent_Qdrant_MCP_Demo/RAG_AIAgent_Qdrant_MCP_Demo/Program.cs
+++ RAG_AIAgent_Qdrant_MCP_Demo/RAG_AIAgent_Qdrant_MCP_Demo/Program.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Data;
 using RAG_AIAgent_Qdrant_MCP_Demo.DataLoader;
 using RAG_AIAgent_Qdrant_MCP_Demo.Embeding;
 using RAG_AIAgent_Qdrant_MCP_Demo.SemanticKernelCore;
+using RAG_AIAgent_Qdrant_MCP_Demo.VectorStore;
 
 
 var builder = new ConfigurationBuilder()
@@ -20,39 +23,53 @@ var chatApiUrl = configuration.GetSection("aiservice:chatcompletion:ollama:uri")
 SemanticKernelService semanticKernelService = new SemanticKernelService();
 Kernel kernel = semanticKernelService.CreateKernelWithAIService(embeddingModel, embeddingApiUrl, chatModel, chatApiUrl);
 
-EmbeddingCore embeddingCore = new EmbeddingCore(kernel);
-embeddingCore.UploadEmbedding(configuration);  
+// For adding the embedding generator to the kernel 
+//EmbeddingCore embeddingCore = new EmbeddingCore(kernel);
+//embeddingCore.UploadEmbedding(configuration);
 
-// Read the values
-
-//var qdranturi = configuration.GetSection("vectorstore:qdrant:uri").Get<string>();
-//var apikey = configuration.GetSection("vectorstore:qdrant:apikey").Get<string>();
-
-//var embeddingModel = configuration.GetSection("aiservice:embedding:ollama:model").Get<string>();
-//var embeddingApiUrl = configuration.GetSection("aiservice:embedding:ollama:uri").Get<string>();
-
-//var chatModel = configuration.GetSection("aiservice:chatcompletion:ollama:model").Get<string>();
-//var chatApiUrl = configuration.GetSection("aiservice:chatcompletion:ollama:uri").Get<string>();
 
 
 
 //////
 ///
 
-//string query = string.Empty;
+IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator = kernel.GetRequiredService<IEmbeddingGenerator<string, Embedding<float>>>();
+var qdranturi = configuration.GetSection("vectorstore:qdrant:uri").Get<string>();
+var qdrantapikey = configuration.GetSection("vectorstore:qdrant:apikey").Get<string>();
+var collectionname = configuration.GetSection("files:pdffiles:collectionname").Get<string>();
 
-//while (query.ToLower() != "exit")
-//{
-//    Console.WriteLine("Enter your query (or type 'exit' to quit): ");
-//    Console.WriteLine();
-//    query = Console.ReadLine();
-//    if (query == "exit")
-//    {
-//        break;
-//    }
+QdrantVectorStoreService qdrantVectorStoreService = new  QdrantVectorStoreService(embeddingGenerator, qdranturi, qdrantapikey, collectionname);
 
-//    var result = await kernel.InvokePromptAsync(query);
-//    Console.WriteLine($"AI Response: {result}");
-//    Console.WriteLine();
-//}
+#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var textSearch = new VectorStoreTextSearch<TextSnippet>(qdrantVectorStoreService.Collection, embeddingGenerator);
+#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+var searchPlugin = textSearch.CreateWithGetTextSearchResults("SearchPlugin");
+
+kernel.Plugins.Add(searchPlugin);
+
+PromptExecutionSettings promptExecutionSettings = new PromptExecutionSettings
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+KernelArguments arguments = new(promptExecutionSettings);
+
+
+string query = string.Empty;
+
+while (query.ToLower() != "exit")
+{
+    Console.WriteLine("Enter your query (or type 'exit' to quit): ");
+    Console.WriteLine();
+    query = Console.ReadLine();
+    if (query == "exit")
+    {
+        break;
+    }
+
+    var result = await kernel.InvokePromptAsync(query, arguments);
+    Console.WriteLine($"AI Response: {result}");
+    Console.WriteLine();
+}
 
