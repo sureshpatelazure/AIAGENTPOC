c49cdfc22d686630d8321521a742a0d24fcc5945
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-07-05 13:32:30 +0530

    Added Appsetting.json

--- RAG_AIAgent_Qdrant_MCP_Demo/RAG_AIAgent_Qdrant_MCP_Demo/Program.cs
+++ RAG_AIAgent_Qdrant_MCP_Demo/RAG_AIAgent_Qdrant_MCP_Demo/Program.cs
@@ -1,5 +1,25 @@
 ﻿using RAG_AIAgent_Qdrant_MCP_Demo.DataLoader;
+using Microsoft.Extensions.Configuration;
+
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+
+IConfiguration configuration = builder.Build();
+
+// Read the values
+var filePaths = configuration.GetSection("fileconfiguration:pdffileconfiguration:filepath").Get<string[]>();
+var batchSize = configuration.GetValue<int>("fileconfiguration:pdffileconfiguration:batchSize");
 
 PDFLoader pDFLoader = new PDFLoader();
-var pdfdata = pDFLoader.LoadPDF("C:\\GenAI\\semantic-kernel.pdf", 10).GetAwaiter().GetResult();
-var test = 0;
+
+foreach (var path in filePaths)
+{
+    var pdfdata = pDFLoader.LoadPDF(path, batchSize).GetAwaiter().GetResult();
+}
+
+
+
+
