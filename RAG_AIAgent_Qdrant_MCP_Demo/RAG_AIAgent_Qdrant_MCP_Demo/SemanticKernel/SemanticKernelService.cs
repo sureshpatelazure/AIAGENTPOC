5e0c480435cfb717c9eee18d9b6a5fd80ec843b3
sureshpatelgcp <sureshpatelazure@gmail.com>
2025-07-12 20:47:58 +0530

    Update SemanticKernelService.cs

--- RAG_AIAgent_Qdrant_MCP_Demo/RAG_AIAgent_Qdrant_MCP_Demo/SemanticKernel/SemanticKernelService.cs
+++ RAG_AIAgent_Qdrant_MCP_Demo/RAG_AIAgent_Qdrant_MCP_Demo/SemanticKernel/SemanticKernelService.cs
@@ -11,12 +11,12 @@ namespace RAG_AIAgent_Qdrant_MCP_Demo.SemanticKernel
 
              #pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
             // builder.AddOllamaChatCompletion(chatModel, new Uri(chatApiUrl));
-            builder.AddHuggingFaceChatCompletion("google/gemma-2-2b-it", new Uri("https://router.huggingface.co/nebius/"), "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");
+            builder.AddHuggingFaceChatCompletion("google/gemma-2-2b-it", new Uri("https://router.huggingface.co/nebius/"), "");
             #pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
             #pragma warning disable SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
             builder.Services.AddOllamaEmbeddingGenerator(embeddingModel, new Uri(embeddingApiUrl));
-            //builder.Services.AddHuggingFaceEmbeddingGenerator("mixedbread-ai/mxbai-embed-large-v1", new Uri("https://router.huggingface.co/hf-inference/models/mixedbread-ai/mxbai-embed-large-v1/pipeline/feature-extraction/"), "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");     
+            //builder.Services.AddHuggingFaceEmbeddingGenerator("mixedbread-ai/mxbai-embed-large-v1", new Uri("https://router.huggingface.co/hf-inference/models/mixedbread-ai/mxbai-embed-large-v1/pipeline/feature-extraction/"), "");     
             #pragma warning restore SKEXP0070 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
             return builder.Build();
